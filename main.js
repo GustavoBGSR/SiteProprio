548f6ea5732e0168042a84e2c84191f63e031192
Borges <107554951+GustavoBGSR@users.noreply.github.com>
2022-08-12 18:30:46 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 800,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+});
+
